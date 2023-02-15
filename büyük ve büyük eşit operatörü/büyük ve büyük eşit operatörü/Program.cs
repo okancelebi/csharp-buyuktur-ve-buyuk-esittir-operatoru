8fd31f32a2c3cc2066fad3086a6bd82194bbf433
Okan Çelebi <okancelebi06@gmail.com>
2023-02-15 23:14:37 +0300

    Add files via upload

---
+++ büyük ve büyük eşit operatörü/büyük ve büyük eşit operatörü/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace büyük_ve_büyük_eşit_operatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Büyüktür operatörü : >
+            //Büyük ve eşittir operatörü : >=
+            int yas = 21;
+            bool kontrolet = yas < 18;
+            int yas2 = 21;
+            bool kontrolet2 = yas2 >= 21;
+            Console.WriteLine(kontrolet);
+            Console.WriteLine(kontrolet2);
+            Console.ReadLine();
+        }
+    }
+}
